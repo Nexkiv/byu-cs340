837b56f92bd13f4fd61c383d72bc02ac4fab796d
nexkiv <kevin@karatassos.com>
2025-09-17 23:45:02 -0600

    [edit] started work on index.ts

--- image-editor/src/index.ts
+++ image-editor/src/index.ts
@@ -1,3 +1,10 @@
 const {readFileSync, openSync, writeSync, closeSync} = require('fs');
 
-console.log('Hello World');
+const args: readonly string[] = process.argv.slice(2);
+
+if (args.length < 2) {
+    // The function call does not contain enough arguments
+}
+
+const inputFile: string | undefined = args[0];
+const outputFile: string | undefined = args[1];
