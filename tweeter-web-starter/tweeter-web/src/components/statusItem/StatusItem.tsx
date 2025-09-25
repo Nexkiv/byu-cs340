f4d3217f1f8c8b2a9265628372a229a2a8331e27
nexkiv <kevin@karatassos.com>
2025-09-25 03:53:11 -0600

    [Create] MessageHooks.ts

--- tweeter-web-starter/tweeter-web/src/components/statusItem/StatusItem.tsx
+++ tweeter-web-starter/tweeter-web/src/components/statusItem/StatusItem.tsx
@@ -1,10 +1,9 @@
 import { Link, useNavigate } from "react-router-dom";
 import { AuthToken, FakeData, Status, User } from "tweeter-shared";
 import Post from "./Post";
-import { ToastType } from "../toaster/Toast";
 import { useContext } from "react";
-import { ToastActionsContext } from "../toaster/ToastContexts";
 import { UserInfoActionsContext, UserInfoContext } from "../userInfo/UserInfoContexts";
+import { useMessageActions } from "../toaster/MessageHooks";
 
 interface Props {
   status: Status;
@@ -12,7 +11,7 @@ interface Props {
 }
 
 const StatusItem = (props: Props) => {
-  const { displayToast } = useContext(ToastActionsContext);
+  const { displayErrorMessage } = useMessageActions();
   const { displayedUser, authToken } = useContext(UserInfoContext);
   const { setDisplayedUser } = useContext(UserInfoActionsContext);
 
@@ -33,10 +32,8 @@ const StatusItem = (props: Props) => {
           }
         }
       } catch (error) {
-        displayToast(
-          ToastType.Error,
-          `Failed to get user because of exception: ${error}`,
-          0
+        displayErrorMessage (
+          `Failed to get user because of exception: ${error}`
         );
       }
     };
