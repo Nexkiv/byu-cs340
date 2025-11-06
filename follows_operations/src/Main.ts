92347d37f0c3196b91952d84e7590c60cf54d386
nexkiv <kevin@karatassos.com>
2025-11-05 21:37:27 -0700

    [Created] DynamoDB Exercise Part 1

---
+++ follows_operations/src/Main.ts
@@ -0,0 +1,122 @@
+import { Follow } from "./entity/Follow";
+import { FollowsDAO } from "./dao/FollowsDAO";
+
+const handles = [
+  "@DonaldDuck",
+  "@HueyDuck",
+  "@DeweyDuck",
+  "@LouieDuck",
+  "@DellaDuck",
+  "@LudwigVonDrake",
+  "@MatildaDuck",
+  "@GladstoneGander",
+  "@FethryDuck",
+  "@AbnerDuck",
+  "@GusGoose",
+  "@ScroogeMcDuck",
+  "@HortenseMcDuck",
+  "@QuackmoreDuck",
+  "@GoostaveGander",
+  "@DaphneDuck",
+  "@LulubelleLoon",
+  "@EiderDuck",
+  "@FannyCoot",
+  "@LukeGoose",
+  "@CuthbertCoot",
+  "@DownyODrake",
+  "@FergusMcDuck",
+  "@HumperdinkDuck",
+  "@ElviraCoot",
+];
+
+const names = [
+  "Donald Duck",
+  "Huey Duck",
+  "Dewey Duck",
+  "Louie Duck",
+  "Della Duck",
+  "Ludwig VonDrake",
+  "Matilda Duck",
+  "Gladstone Gander",
+  "Fethry Duck",
+  "Abner Duck",
+  "Gus Goose",
+  "Scrooge McDuck",
+  "Hortense McDuck",
+  "Quackmore Duck",
+  "Goostave Gander",
+  "Daphne Duck",
+  "Lulubelle Loon",
+  "Eider Duck",
+  "Fanny Coot",
+  "Luke Goose",
+  "Cuthbert Coot",
+  "Downy O'Drake",
+  "Fergus McDuck",
+  "Humperdink Duck",
+  "Elvira Coot",
+];
+
+const specificHandle = "@MarkBeaks";
+const specificName = "Marcus Beaks";
+
+const specificFollow = new Follow(specificHandle, "@DonaldDuck");
+
+class Main {
+  async run() {
+    const followsDao = new FollowsDAO();
+
+    // “Put” 25 items into the “follows” table all with the same follower:
+    for (let i = 0; i < handles.length; i++) {
+      await followsDao.putFollow(
+        new Follow(specificHandle, handles.at(i)!, specificName, names.at(i)!)
+      );
+    }
+
+    // "Put" 25 more items into the "follows" table, this time all with the same followee:
+    for (let i = 0; i < handles.length; i++) {
+      await followsDao.putFollow(
+        new Follow(handles.at(i)!, specificHandle, names.at(i)!, specificName)
+      );
+    }
+
+    // “Get” one of the items from the “follows” table using its primary key
+    let follow = await followsDao.getFollow(specificFollow);
+    if (!!follow) {
+      console.log(follow.toString());
+    } else {
+      console.log("Error: Unable to read in follow information.");
+    }
+
+    // “Update” the “follower_name” and “followee_name” attributes of one of the items in the “follows” table
+    await followsDao.updateFollowNames(
+      specificHandle,
+      "@DonaldDuck",
+      "Mark Beaks",
+      "The Real Donald Duck"
+    );
+
+    follow = await followsDao.getFollow(specificFollow);
+    if (!!follow) {
+      console.log(follow.toString());
+    } else {
+      console.log("Error: Unable to read in follow information.");
+    }
+
+    // “Delete” one of the items in the “follows” table using its primary key
+    const scroogeFollow = new Follow("@ScroogeMcDuck", specificHandle);
+    await followsDao.deleteFollow(scroogeFollow);
+    follow = await followsDao.getFollow(scroogeFollow);
+    if (!!follow) {
+      console.log("Error: Unable to remove the follow relationship.");
+    } else {
+      console.log("Successfully removed the follow relationship.");
+    }
+  }
+}
+
+function run() {
+  new Main().run();
+}
+
+run();
---
+++ follows_operations/src/dao/FollowsDAO.ts
@@ -0,0 +1,163 @@
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  GetCommand,
+  PutCommand,
+  QueryCommand,
+  UpdateCommand,
+} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DataPage } from "../entity/DataPage";
+import { Follow } from "../entity/Follow";
+
+const PAGE_LIMIT = 10;
+
+export class FollowsDAO {
+  readonly tableName = "follows";
+  readonly indexName = "follows_index";
+  readonly followerHandleAttr = "follower_handle";
+  readonly followerNameAttr = "follower_name";
+  readonly followeeHandleAttr = "followee_handle";
+  readonly followeeNameAttr = "follower_name";
+
+  private readonly client = DynamoDBDocumentClient.from(new DynamoDBClient());
+
+  /** Retrieve a specific follow relationship */
+  async getFollow(follow: Follow): Promise<Follow | undefined> {
+    const params = {
+      TableName: this.tableName,
+      Key: this.generateFollowItem(follow),
+    };
+    const output = await this.client.send(new GetCommand(params));
+    return output.Item == undefined
+      ? undefined
+      : this.constructFollow(output.Item);
+  }
+
+  /** Put a new follow relationship */
+  async putFollow(follow: Follow): Promise<void> {
+    const params = {
+      TableName: this.tableName,
+      Item: follow,
+    };
+    await this.client.send(new PutCommand(params));
+  }
+
+  /** Delete a follow relationship */
+  async deleteFollow(follow: Follow): Promise<void> {
+    const params = {
+      TableName: this.tableName,
+      Key: this.generateFollowItem(follow),
+    };
+    await this.client.send(new DeleteCommand(params));
+  }
+
+  /** Paginated fetch of users followed by a follower */
+  async getFollowees(
+    follower_handle: string,
+    limit: number = PAGE_LIMIT,
+    lastFollowee?: string
+  ): Promise<DataPage<Follow>> {
+    const params: any = {
+      TableName: this.tableName,
+      KeyConditionExpression: `${this.followerHandleAttr} = :follower_handle`,
+      ExpressionAttributeValues: { ":follower_handle": follower_handle },
+      Limit: limit,
+    };
+    if (lastFollowee !== undefined) {
+      params.ExclusiveStartKey = {
+        [this.followerHandleAttr]: follower_handle,
+        [this.followeeHandleAttr]: lastFollowee,
+      };
+    }
+
+    const items: Follow[] = [];
+    const data = await this.client.send(new QueryCommand(params));
+    const hasMorePages = !!data.LastEvaluatedKey;
+    data.Items?.forEach((item) => items.push(this.constructFollow(item)));
+
+    return new DataPage<Follow>(items, hasMorePages);
+  }
+
+  /** Paginated fetch of followers of a given user by index */
+  async getFollowers(
+    followee_handle: string,
+    limit: number = PAGE_LIMIT,
+    lastFollower?: string
+  ): Promise<DataPage<Follow>> {
+    const params: any = {
+      TableName: this.tableName,
+      IndexName: this.indexName,
+      KeyConditionExpression: `${this.followeeHandleAttr} = :followee_handle`,
+      ExpressionAttributeValues: { ":followee_handle": followee_handle },
+      Limit: limit,
+    };
+    if (lastFollower !== undefined) {
+      params.ExclusiveStartKey = {
+        [this.followeeHandleAttr]: followee_handle,
+        [this.followerHandleAttr]: lastFollower,
+      };
+    }
+
+    const items: Follow[] = [];
+    const data = await this.client.send(new QueryCommand(params));
+    const hasMorePages = !!data.LastEvaluatedKey;
+    data.Items?.forEach((item) => items.push(this.constructFollow(item)));
+
+    return new DataPage<Follow>(items, hasMorePages);
+  }
+
+  /** Update the associated names in a follow relationship */
+  async updateFollowNames(
+    follower_handle: string,
+    followee_handle: string,
+    follower_name?: string,
+    followee_name?: string
+  ): Promise<void> {
+    const updateExpressions: string[] = [];
+    const expressionAttrValues: Record<string, any> = {};
+
+    if (follower_name !== undefined) {
+      updateExpressions.push("follower_name = :follower_name");
+      expressionAttrValues[":follower_name"] = follower_name;
+    }
+
+    if (followee_name !== undefined) {
+      updateExpressions.push("followee_name = :followee_name");
+      expressionAttrValues[":followee_name"] = followee_name;
+    }
+
+    if (updateExpressions.length === 0) {
+      throw new Error("No attributes provided to update");
+    }
+
+    const params = {
+      TableName: this.tableName,
+      Key: {
+        follower_handle,
+        followee_handle,
+      },
+      UpdateExpression: "SET " + updateExpressions.join(", "),
+      ExpressionAttributeValues: expressionAttrValues,
+    };
+
+    await this.client.send(new UpdateCommand(params));
+  }
+
+  /** Helper to create the key item for a Follow */
+  private generateFollowItem(follow: Follow) {
+    return {
+      follower_handle: follow.follower_handle,
+      followee_handle: follow.followee_handle,
+    };
+  }
+
+  private constructFollow(item: any): Follow {
+    return new Follow(
+      item.follower_handle,
+      item.followee_handle,
+      item.follower_name,
+      item.followee_name
+    );
+  }
+}
